2528b60e882c6e30505dfddb10d04321e61f6a3a
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-26 00:10:45 +0530

    setup body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/server-config");
 
 const setUpAndStartServer = () =>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    
     app.listen(PORT, ()=>{
         console.log(`Server is up and running on PORT: ${PORT}`);
     });
