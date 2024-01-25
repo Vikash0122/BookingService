4583a2caeb654c17e740adc4f290a7e4c75df45f
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-26 00:23:02 +0530

    Basic router and model added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const apiRoutes = require('./routes/index');
 
 const { PORT } = require("./config/server-config");
 
 const setUpAndStartServer = () =>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    
+    app.use("/api", apiRoutes);
+
     app.listen(PORT, ()=>{
         console.log(`Server is up and running on PORT: ${PORT}`);
     });
